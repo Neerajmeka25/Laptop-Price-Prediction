45817e329466ce7b3a6dd57cd1206c20fd05334c
Neeraj Subhash Meka <neeraj2512002@gmail.com>
2022-11-09 00:28:30 +0530

    Updates

---
+++ precache-manifest.4a1895dffd2b272721d12a39b8525929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3ef0826dcf5d4214827bbdf4496b9d",
+    "url": "/laptop-nation/index.html"
+  },
+  {
+    "revision": "1272a6cfa2e57f36317b",
+    "url": "/laptop-nation/static/js/2.e99dd161.chunk.js"
+  },
+  {
+    "revision": "70af1bffd51a7281193fa8f9b7319597",
+    "url": "/laptop-nation/static/js/2.e99dd161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d556e5917647176c57f5",
+    "url": "/laptop-nation/static/js/main.457bf22c.chunk.js"
+  },
+  {
+    "revision": "d1fb28474eb296736358",
+    "url": "/laptop-nation/static/js/runtime-main.d491d938.js"
+  },
+  {
+    "revision": "ad5b8e88fbfa63b81f7a482412c46ed3",
+    "url": "/laptop-nation/static/media/commerce.ad5b8e88.png"
+  }
+]);
